e23170a6cf242e64bb56a84f2092eea0d205d5e5
Prince242007 <princevaghasiya204@gmail.com>
2025-07-25 18:22:42 +0530

    Nearest palindrone number

---
+++ nearsetpalin.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int Is_palindrome(int r1) {
+    int reverse = 0;
+    int palindrome = r1;
+    while (r1 > 0) {
+        reverse = reverse * 10 + r1 % 10;
+        r1 /= 10;
+    }
+    if (palindrome == reverse) {
+        return palindrome;
+    } else {
+        return 0;
+    }
+}
+
+int Rev_num(string s) {
+    int r = 0, r1 = 0, val1, val2;
+
+
+    for (int i = s.size() - 1; i >= 0; i--) {
+        r = r * 10 + (s[i] - '0');  
+    }
+
+    
+    for (; r > 0; r /= 10) {
+        r1 = r1 * 10 + (r % 10);
+    }
+
+    
+    for (int i = r1, j = r1; i >= 0 || j < INT_MAX; i--, j++) {
+        val1 = Is_palindrome(i);
+        val2 = Is_palindrome(j);
+        if (val1 && val2) {
+            if ((r1 - val1) <= (val2 - r1)) return val1;
+            else return val2;
+        }
+        if (val1) return val1;
+        if (val2) return val2;
+    }
+
+    return 0;
+}
+
+int main(int argc, char const *argv[]) {
+    string s;
+    cout << "Enter the string = ";
+    cin >> s;
+    int number = Rev_num(s);
+
+    
+    cout << "Nearest palindrome is: " << number << endl;
+    return 0;
+}
