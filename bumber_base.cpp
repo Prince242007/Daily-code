2467d5d82224a4d2b3bee3d89f5ae6fb2c485927
Prince242007 <princevaghasiya204@gmail.com>
2025-07-26 11:23:35 +0530

    Change in

--- bumber_base.cpp
+++ bumber_base.cpp
@@ -30,3 +30,38 @@ int main() {
 
     return 0;
 }
+
+
+
+// This code for only base 1-10
+/*
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n, b;
+    cout << "Enter the number in decimal: ";
+    cin >> n;
+    cout << "Enter the base to convert to (≤10): ";
+    cin >> b;
+
+    if (b > 10) {
+        cout << "Cannot represent base > 10 in int. Use string instead." << endl;
+        return 0;
+    }
+
+    int result = 0, place = 1;
+
+    while (n > 0) {
+        int r = n % b;
+        result += r * place;
+        place *= 10;
+        n /= b;
+    }
+
+    cout << "Converted number = " << result << endl;
+
+    return 0;
+}
+    */
+
