8ac5c75e1ef40ed3c3c0bdbfb4f506d36209515e
Prince242007 <princevaghasiya204@gmail.com>
2025-07-29 18:22:25 +0530

    Basic program of c

---
+++ College/1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    printf("C Programming.");
+    printf("\nPrince Vaghasiya");
+    printf("\nMavdi - rajkot- 360004");
+    return 0;
+}
