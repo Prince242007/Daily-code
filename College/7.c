dd95c7aab864bb7d169b6be102cb67923fb2f7a8
Prince242007 <princevaghasiya204@gmail.com>
2025-07-29 18:24:01 +0530

    Code of c

---
+++ College/7.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(int argc, char const *argv[])
+{
+    int a,b;
+    printf("Enter the value a & b =");
+    scanf("%d %d",&a,&b);
+    printf("\nSum=%d",a+b);
+    printf("\nSub=%d",a-b);
+    printf("\nMul=%d",a*b);
+    printf("\nDiv=%d",a/b);
+    return 0;
+}
