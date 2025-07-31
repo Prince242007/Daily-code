ecb1b8eb6064a29476664610a7554a624e60a996
Prince242007 <princevaghasiya204@gmail.com>
2025-07-31 19:35:40 +0530

    Daily c code

---
+++ College/13.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    float  r;
+    printf("Enter the radius = ");
+    scanf("%f",&r);
+    printf("area = %f",3.14*r*r);
+    return 0;
+}
