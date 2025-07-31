293a300bd538eda8f3b2f37d6c72e9761a0495c2
Prince242007 <princevaghasiya204@gmail.com>
2025-07-31 19:35:56 +0530

    Daily c code

---
+++ College/14.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    float  b,l;
+    printf("Enter the base and Length = ");
+    scanf("%f %f",&b,&l);
+    printf("area = %f",0.5*b*l);
+    return 0;
+}
