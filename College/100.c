7f042294c3f3dd77845ab82a0b9653622d1fee30
Prince242007 <princevaghasiya204@gmail.com>
2025-08-21 23:08:20 +0530

    Daily c code

---
+++ College/100.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int n;
+    printf("Enter the Number = ");
+    scanf("%d",&n);
+    for (int i = 1; i <=n; i++)
+    {
+        for (int j = 1; j <=n-i+1;j++)
+        {
+            printf("%d",j);
+        }
+        printf("\n");
+        
+    }
+    
+    return 0;
+}
