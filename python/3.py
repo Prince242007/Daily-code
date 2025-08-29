8f33c15df621d906f11aa5c2fd1d94552ea6c8d0
Prince242007 <princevaghasiya204@gmail.com>
2025-08-29 15:24:20 +0530

    Python lab 3 code assignment

---
+++ python/3.py
@@ -0,0 +1,133 @@
+# Name = "ABCBA"
+# Reverse_name = Name[::-1]
+# print(Reverse_name)
+# if(Reverse_name == Name):
+#     print("String is palindrome")
+# else :
+#     print("String is not palindrome")    
+
+# sTring = "1234"
+# sTring1 = "1234E"
+# if(sTring.isnumeric()) :
+#     print("String has only numeric value")
+# else :
+#     print("String has not only numeric value.")
+# if(sTring1.isnumeric()) :
+#     print("String has only numeric value")
+# else :
+#     print("String has not only numeric value.")
+
+
+# sentence = "Python is a powerful programming language"
+# words = sentence.split()
+# longest = max(words, key=len)
+# print(longest)  
+
+
+# sentence = "Python is fun to learn"
+# words = sentence.split()
+# last_word = words[-1]
+# print(last_word)  
+
+
+# string1 = "ICT Department"
+# print(string1)
+# string1 = ' ICT Department '
+# print(string1)
+# string2 = '3EK1'
+# print(string2 [1])
+# string3 = 'ICT Department'
+# print(string3 [-4]) 
+
+# string4 = 'ICT Department'
+# # access character from 1st index to 3rd index
+# print(string4[1:4])  
+# print(string4[:2])
+# print(string4[2:])
+
+# message = 'ICT Department'
+# message[0] = 'H'
+# print(message)
+# message = 'ICT'
+# # assign new string to message variable
+# message = 'ICT Department'
+# print(message)
+
+
+
+# message = """
+# ICT
+# Department
+# 3EK1
+# """
+# print(message)
+
+# str1 = "ICT"
+# str2 = "Department"
+# str3 = "3EK1"
+# # compare str1 and str2
+# print(str1 == str2)
+# # compare str1 and str3
+# print(str1 == str3)
+
+# greet = "ICT"
+# name = "Department"
+# # using + operator
+# result = greet + name
+# print(result)
+
+# greet = 'ICT'
+# # count length of greet string
+# print(len(greet))
+
+# print('a' in 'program') 
+# print('at' not in 'battle') 
+
+# message = 'python is fun'
+# # convert message to uppercase
+# print(message.upper())
+
+# message = 'PYTHON IS FUN'
+# # convert message to lowercase
+# print(message.lower())
+
+# text = 'CE Department'
+# replaced_text = text.replace('CE', 'ICT')
+# print(replaced_text)
+
+
+# message = 'Python is a fun programming language'
+# # check the index of 'fun'
+# print(message.find('fun'))
+
+# title = 'Python Programming   '
+# result = title.rstrip()
+# print(result)
+
+
+# text = 'Python is fun'
+# # split the text from space
+# print(text.split())
+
+# message = 'Python is fun'
+# # check if the message starts with Python
+# print(message.startswith('Python'))
+
+# pin = "523"
+# # checks if every character of pin is numeric 
+# print(pin.isnumeric())
+
+# text = 'Python is fun'
+# # find the index of is
+# result = text.index('is')
+# print(result)
+
+
+# name = 'Cathy'
+# country = 'UK'
+# print(f'{name} is from {country}')
+
+str = "This is a \n normal string example" 
+print(str) 
+raw_str = r"This is a \n raw string example" 
+print(raw_str)
