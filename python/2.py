d967edfd20eea18fe0966b2e11d47485f09fa61a
Prince242007 <princevaghasiya204@gmail.com>
2025-09-01 18:57:32 +0530

    Daily python code

---
+++ python/2.py
@@ -0,0 +1,111 @@
+# a=int(input("Enter the length = "))
+# b=int(input("Enter the width = "))
+# print("Area of rectengle = ",(a*b))
+# print("Perimeter of rectengle = ",(2*(a+b)))
+
+
+
+# c=int(input("Enter the number = "))
+# if(c%2==0) :
+#     print("The umber is even.")
+
+# else :
+#     print("The number is odd.")
+
+
+# d=int(input("Enter the length = "))
+# e=int(input("Enter the width = "))
+
+# print("Area of cube = ",(d*6*d))
+# print("Perimeter of cube = ",((d*e)**3))
+
+# x=int(input("Enter the x = "))
+# y=int(input("Enter the y = "))
+# z=(x+y)*(x-y)
+# print(z)
+
+# x=int(input("Enter the x = "))
+# y=int(input("Enter the y = "))
+# z=(x+y)*(x+y)-2*x*y
+# print(z)
+
+# c=int(input("Enter the celsius = "))
+# f=(9/5)*c  +32
+# print(f)
+
+
+
+# num1, num2 = 10, 30
+# sum= num1+num2
+# print("The sum of",num1,"and",num2,"is:",sum)
+
+
+
+# num1, num2 = 10, 30
+# sub= num1-num2
+# print("The subtraction of",num1,"and",num2,"is:",sub)
+
+
+# num1, num2 = 10, 30
+# product= num1*num2
+# print("The product of",num1,"and",num2,"is:",product)
+
+
+# num1, num2 = 10, 30
+# div = num1/num2
+# print("The division of",num1,"and",num2,"is:",div)
+
+
+# num1, num2 = 10, 30
+# rem = num1%num2
+# print("The reminder of",num1,"and",num2,"is:",rem)
+
+
+# num1, num2 = 10, 3
+# exp = num1**num2
+# print("The exponentiation of",num1,"and",num2,"is:",exp)
+
+
+# num1, num2 = 10, 3
+# floordiv = num1//num2
+# print("The Floor Division of",num1,"and",num2,"is:",floordiv)
+
+
+# x = 8
+# y = 3
+# mod = x % y
+# print (mod)
+
+
+# a = -5
+# b = 2
+# res1 = a % b
+# print (res1)
+
+# m = 5
+# n = -2
+# res2 = m % n
+# print (res2)
+
+
+
+# e = -5
+# f = -2
+# res3 = e % f
+# print (res3)
+
+# print (((5 + 4) / 3) * 2)
+
+
+# x = 3
+# y = 4
+# z = 6
+# print(x*y//z)
+# print(x*(y//z))
+
+
+x = 2
+y = 3
+z = 2
+print(x**y**z)
+print((x**y)**z)
