1e454a94dd8eefca617712ff4a7747d6837d88c2
Prince242007 <princevaghasiya204@gmail.com>
2025-09-15 09:44:01 +0530

    Daily leetcode

---
+++ l_412.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the number of elements: ";
+    cin>>n;
+    vector<string> answer;
+    for(int i=1;i<=n;i++){
+        if(i%3==0 && i%5==0){
+            answer.push_back("FizzBuzz");
+        }
+        else if(i%3==0){
+            answer.push_back("Fizz");
+        }
+        else if(i%5==0){
+            answer.push_back("Buzz");
+        }
+        else{
+            answer.push_back(to_string(i));
+        }
+    }
+    for (int i = 0; i < answer.size(); i++)
+    {
+        cout<<answer[i]<<" ";
+    }
+    return 0;
+}
