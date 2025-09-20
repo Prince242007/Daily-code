9db034cdff449a3fc2ae99312494f4b0901e6c89
Prince242007 <princevaghasiya204@gmail.com>
2025-09-20 13:51:54 +0530

    Daily leetcode 242 code '

---
+++ l_242.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public :
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+        if(s==t)
+        cout<<"true";
+        else
+        cout<<"False";
+    }
+};
+int main(){
+    Solution s;
+    string string1,string2;
+    cout<<"Enter the string 1 :-";
+    cin>> string1;
+    cout<<"Enter the string 2 :-";
+    cin>> string2;
+    s.isAnagram(string1,string2);
+    
+    return 0;
+}
