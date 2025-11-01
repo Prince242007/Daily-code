2a43a1139d01bd095f96ecfbb8a25e5acac8842c
Prince242007 <princevaghasiya204@gmail.com>
2025-11-01 15:56:53 +0530

    Daily leetcode optimise 1688 code

--- l_1688.cpp
+++ l_1688.cpp
@@ -1,31 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Solution{
-public :
+class Solution {
+public:
     int numberOfMatches(int n) {
-        int answer ,matches=0;
-        while(n>0)
-        {
-            if(n==2)
-            {
-                 matches +=1;
-                 return matches;
-            }
-            if(n%2==0)
-            {
-                matches+=n/2;
-                n/=2;
-            }
-            else
-            {
-                matches+=(n-1)/2;
-                n=(n-1)/2+1;
+        int matches = 0;
+        while (n > 1) { // loop until one team remains
+            if (n % 2 == 0) {
+                matches += n / 2;
+                n /= 2;
+            } else {
+                matches += (n - 1) / 2;
+                n = (n - 1) / 2 + 1;
             }
         }
         return matches;
     }
 };
+
 int main(){
     Solution s;
     int n; 
