ece17c25c5900bb102e1ebea7d5db78d4e017e6a
Prince242007 <princevaghasiya204@gmail.com>
2025-11-01 15:57:20 +0530

    Daily leetcode 1588  code

--- l_1588.cpp
+++ l_1588.cpp
@@ -1,59 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 class Solution
 {
 public:
-    // int sumOddLengthSubarrays(vector<int>& arr) {
-    //     int sum=0;
-    //     for (int i = 0; i < arr.size(); i++)
-    //     {
-    //         for (int j = i; j < arr.size(); j++)
-    //         {
-    //             if((j-i+1)%2!=0)
-    //             {
-    //                 int k=i;
-    //                 while(k<=j)
-    //                 {
-    //                     sum = sum+arr[k];
-    //                     k++;
-    //                 }
-    //             }
-    //         }
-
-    //     }
-    //     return sum ;
-    // }
-    int sumOddLengthSubarrays(vector<int> &arr)
-    {
-        int len = arr.size();
-        int end, sum = 0;
-        if (len % 2 == 0)
+    int sumOddLengthSubarrays(vector<int>& arr) {
+        int sum=0;
+        for (int i = 0; i < arr.size(); i++)
         {
-            end = len / 2;
-        }
-        else
-        {
-            end = len / 2 + 1;
-        }
-        int multi = end;
-        for (int i = 0; i < len; i++)
-        {
-
-            if (i < end)
+            for (int j = i; j < arr.size(); j++)
             {
-                sum += arr[i] * multi;
-                if(i==len/2)
+                if((j-i+1)%2!=0)
                 {
-                    multi--;
+                    int k=i;
+                    while(k<=j)
+                    {
+                        sum = sum+arr[k];
+                        k++;
+                    }
                 }
-                multi++;
             }
-            else
-            {
-                multi--;
-                sum += arr[i] * multi;
-            }
+
         }
         return sum ;
     }
@@ -61,8 +27,7 @@ public:
 int main()
 {
     Solution s;
-    vector<int> arr = { 10,11,12};
+    vector<int> arr = {10, 11, 12};
     cout << s.sumOddLengthSubarrays(arr);
-
     return 0;
 }
