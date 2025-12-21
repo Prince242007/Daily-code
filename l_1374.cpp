cbb634ff696ff4746bb4d78e91237a365e181db8
Prince242007 <princevaghasiya204@gmail.com>
2025-12-21 13:11:14 +0530

    Daily leetcode 1374 code

---
+++ l_1374.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public :
+    string generateTheString(int n) {
+        if (n % 2 == 1) {
+            return string(n, 'a');
+        } else {
+            return string(n - 1, 'a') + 'b';
+        }
+    }
+};
+int main(){
+    Solution s;
+    int n;
+    cout<<"Enter the  n :- ";
+    cin>>n;
+    cout<<s.generateTheString(n);
+    return 0;
+}
