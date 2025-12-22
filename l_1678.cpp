3339d954f1dc5202d2bb49d53705d3f5246cf8fd
Prince242007 <princevaghasiya204@gmail.com>
2025-12-22 19:17:32 +0530

    Daily leetcode 1678 code

---
+++ l_1678.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    string interpret(string command)
+    {
+        string answer;
+        for (int i = 0; i < command.length(); i++)
+        {
+            if (command[i] == '(' && command[i + 1] == ')')
+            {
+                answer += 'o';
+            }
+            else if (command[i] != '(' && command[i] != ')')
+            {
+                answer += command[i];
+            }
+            else
+            {
+                continue;
+            }
+        }
+        return answer;
+    }
+};
+int main()
+{
+    Solution s;
+    string command;
+    cout << "Enter the command :- ";
+    cin >> command;
+    cout << s.interpret(command);
+    return 0;
+}
