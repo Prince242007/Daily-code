fd3407b7d8a00c6173a8acb63a695314284131bf
Prince242007 <princevaghasiya204@gmail.com>
2026-01-20 22:01:09 +0530

    Python viva question

---
+++ python/viva.py
@@ -0,0 +1,9 @@
+# import numpy as np
+# arr1 = np.arange(1, 10).reshape(3, 3)
+# print(arr1)
+# arr2= arr1 **2
+# print (arr2)
+
+
+import py_compile as p
+p.compile(r'D:/college sem 3 pdf/Github/daily-code/python/viva.py')
