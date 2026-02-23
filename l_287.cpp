9205384f9799a88dadab34533208dbb85176b745
Prince242007 <princevaghasiya204@gmail.com>
2026-02-23 19:32:38 +0530

    Daily leetcode 287 code

---
+++ l_287.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int findDuplicate(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size()-1; i++)
+        {
+            if(nums[i] == nums[i+1])
+            {
+                return nums[i];
+            }
+        }
+        return 0;
+        
+    }
+};
+int main()
+{
+    Solution s;
+    vector<int> nums = {1,3,4,2,2};
+    cout << s.findDuplicate(nums);
+    return 0;
+}
