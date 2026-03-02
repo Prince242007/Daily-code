08d5c5f5f8a402447e226b7a9ca0a85360aa379e
Prince242007 <princevaghasiya204@gmail.com>
2026-03-02 21:54:44 +0530

    Daily leetcode 1475 code

---
+++ l_1475.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public :
+    vector<int> finalPrices(vector<int>& prices) {
+        for (int i = 0; i < prices.size()-1; i++)
+        {
+            for (int j = i+1; j < prices.size(); j++)
+            {
+                if(j>i && prices[j]<=prices[i])
+                {
+                    prices[i] = prices[i]-prices[j];
+                    break;
+                }
+            }
+            
+        }
+        return prices;
+        
+    }
+};
+int main(){
+    Solution s;
+    vector<int> price={8,4,6,2,3};
+    vector<int> ans=s.finalPrices(price);
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout<<ans[i]<<" ";
+    }
+    
+    return 0;
+}
