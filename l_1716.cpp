f2130d7f956d7ef9477b89efb3e42b4818c48ee5
Prince242007 <princevaghasiya204@gmail.com>
2026-03-19 21:37:16 +0530

    Daily leetcode 1716 code

---
+++ l_1716.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public :
+    int totalMoney(int n) {
+        int ans = 0 ,j=8,i=1,k=1;
+        for (int count = 1; count <=n;count++)
+        {
+            ans+=i;
+            i++;
+            if(count%7==0)
+            {
+                k++;
+                i=k;
+            }
+        }
+        return ans;    
+    }
+};
+int main(){
+    Solution s;
+    int n;
+    cout<<"Enter the  n :- ";
+    cin>>n;
+    cout<<s.totalMoney(n);
+    return 0;
+}
