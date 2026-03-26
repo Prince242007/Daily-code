b825592a7e1b723ecd36311a9b418c139507f737
Prince242007 <princevaghasiya204@gmail.com>
2026-03-26 13:24:38 +0530

    Daily leetcode 553 code

---
+++ l_553.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public :
+    string optimalDivision(vector<int>& nums) {
+        int n = nums.size();
+        
+        if (n == 1) 
+            return to_string(nums[0]);
+        
+        if (n == 2) 
+            return to_string(nums[0]) + "/" + to_string(nums[1]);
+        
+        string result = to_string(nums[0]) + "/(";
+        
+        for (int i = 1; i < n; i++) {
+            result += to_string(nums[i]);
+            if (i != n - 1) 
+                result += "/";
+        }
+        
+        result += ")";
+        return result;
+    }
+};
+int main(){
+    Solution s;
+    vector<int> nums={1000,100,10,2};
+    cout<<s.optimalDivision(nums);
+    return 0;
+}
