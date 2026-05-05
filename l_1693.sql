dac4371922ccbbdf62b5badf96ad6b4ceea53c4c
Prince242007 <princevaghasiya204@gmail.com>
2026-05-05 22:03:30 +0530

    Daily leetcode 1693 code

---
+++ l_1693.sql
@@ -0,0 +1,8 @@
+# leetcode 1693 code
+SELECT 
+    date_id,
+    make_name,
+    COUNT(DISTINCT lead_id) AS unique_leads,
+    COUNT(DISTINCT partner_id) AS unique_partners
+FROM DailySales
+GROUP BY date_id, make_name;
