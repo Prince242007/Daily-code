05debc8120e051d4b9efec07d416ae21dc7c6540
Prince242007 <princevaghasiya204@gmail.com>
2026-05-24 11:51:45 +0530

    Daily leetcode 1317 code

---
+++ l_1317.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public :
+    bool check(int x)
+    {
+        while(x > 0)
+        {
+            if(x % 10 == 0) return false;
+            x /= 10;
+        }
+        return true;
+    }
+
+    vector<int> getNoZeroIntegers(int n) {
+        for (int i = 1; i < n; i++)
+        {
+            int j = n - i;
+            if(check(i) && check(j))
+            {
+                return {i, j};
+            }
+        }
+        return {};
+    }
+};
+int main(){
+    Solution s;
+    int n;
+    cout<<"enter the number  :- ";
+    cin>>n;
+    vector<int> ans=s.getNoZeroIntegers(n);
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout<<ans[i]<<" ";
+    }
+    
+    return 0;
+}
