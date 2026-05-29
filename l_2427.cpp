fa1db300308ee900b5144caeb834433f1d51b9df
Prince242007 <princevaghasiya204@gmail.com>
2026-05-29 20:51:16 +0530

    Daily leetcode 2427 code

---
+++ l_2427.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public :
+    int commonFactors(int a, int b) {
+        int count=0;
+        for (int i = 1; (i <=a && i<=b); i++)
+        {
+            if((a%i==0)&&(b%i==0))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+};
+int main(){
+    Solution s;
+    int a ,b;
+    cout<<"Enter the value of a and b :- ";
+    cin>>a>>b;
+    cout<<s.commonFactors(a,b);
+    return 0;
+}
