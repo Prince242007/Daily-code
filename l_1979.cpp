82f8686cc7d53c59a3620769fdc5283c7135dd52
Prince242007 <princevaghasiya204@gmail.com>
2026-06-02 21:46:43 +0530

    Daily leetcode 1979 code

---
+++ l_1979.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int findGCD(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        int n = nums.size() - 1;
+        int maxy =1;
+        for (int i = 1; i <= nums[0]; i++)
+        {
+            if (nums[0] % i == 0 && nums[n] %i == 0)
+            {
+                maxy = max(maxy, i);
+            }
+        }
+        return maxy;
+    }
+};
+int main()
+{
+    Solution s;
+    vector<int> nums = {7,5,6,8,3};
+    cout << s.findGCD(nums);
+    return 0;
+}
