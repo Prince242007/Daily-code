abe8d51c706728160601b8ff12f49609f0b75e6e
Prince242007 <princevaghasiya204@gmail.com>
2026-07-17 23:13:08 +0530

    Daily leetcode 2582 code

---
+++ l_2582.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+
+        int cycle = n - 1;
+
+        if ((time / cycle) % 2 == 0) {
+            return (time % cycle) + 1;
+        }
+        else {
+            return n - (time % cycle);
+        }
+    }
+};
+
+int main() {
+
+    Solution s;
+
+    int n, time;
+
+    cout << "Enter n and time : ";
+    cin >> n >> time;
+
+    cout << s.passThePillow(n, time);
+
+    return 0;
+}
