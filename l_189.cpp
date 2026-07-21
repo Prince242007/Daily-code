30d7533fd12345cd4db4443bd67f3c44aff0f1bd
Prince242007 <princevaghasiya204@gmail.com>
2026-07-21 12:13:51 +0530

    Daily leetcode 189 code

---
+++ l_189.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k %= n;
+
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
+    }
+};
+int main(){
+    Solution s;
+    vector<int> nums={1,2,3,4,5,6,7};
+    int k;
+    cout<<"Enter the number how many time you want to rotate :- ";
+    cin>>k;
+    s.rotate(nums,k);
+    for(auto x : nums){
+        cout<<x<<" ";
+    }
+    return 0;
+}
