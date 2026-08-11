81b249854052ede390cbb4174a488cb551b926d9
Prince242007 <princevaghasiya204@gmail.com>
2026-08-11 19:53:29 +0530

    Daily leetcode 2996 code

---
+++ l_2996.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int missingInteger(vector<int> &nums)
+    {
+        int sum = nums[0];
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (nums[i] == nums[i - 1] + 1)
+            {
+                sum += nums[i];
+            }
+            else
+            {
+                break;
+            }
+        }
+        while (find(nums.begin(), nums.end(), sum) != nums.end())
+        {
+            sum++;
+        }
+
+        return sum;
+    }
+};
+int main()
+{
+    Solution s;
+    vector<int> nums = {1, 2, 3, 2, 5};
+    cout << s.missingInteger(nums);
+
+    return 0;
+}
