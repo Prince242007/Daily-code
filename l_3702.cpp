b7df82bed859460a8abb18b38d00ce06404f19d6
Prince242007 <princevaghasiya204@gmail.com>
2026-08-15 14:57:41 +0530

    Daily leetcode 3702 code

---
+++ l_3702.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int longestSubsequence(vector<int>& nums) {
+        int n = nums.size();
+        int totalXor = 0;
+        bool nonZero = false;
+
+        for (int x : nums) {
+            totalXor ^= x;
+
+            if (x != 0) {
+                nonZero = true;
+            }
+        }
+
+        if (totalXor != 0)
+            return n;
+
+        if (nonZero)
+            return n - 1;
+
+        return 0;
+    }
+};
+int main(){
+    Solution s;
+    vector<int> nums={2,3,4};
+    cout<<s.longestSubsequence(nums);
+    return 0;
+}
