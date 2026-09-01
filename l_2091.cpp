c6d6efa20ddba60f731f5783a5217e6124fd55d9
Prince242007 <princevaghasiya204@gmail.com>
2026-09-01 22:03:53 +0530

    Daily leetcode 2091 code

--- l_2091.cpp
+++ l_2091.cpp
@@ -35,8 +35,8 @@ public:
 int main()
 {
     Solution s;
-    vector<int> nums={2,10,7,5,4,1,8,6};
-    cout<<s.minimumDeletions(nums);
+    vector<int> num={2,10,7,5,4,1,8,6};
+    cout<<s.minimumDeletions(num);
 
     return 0;
 }
