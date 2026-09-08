c865859d2e5bafba6354f64e86432262f3d89360
Prince242007 <princevaghasiya204@gmail.com>
2026-09-08 14:31:19 +0530

    Daily leetcode 1780 code

---
+++ l_1780.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool checkPowersOfThree(int n) {
+        while (n > 0) {
+            if (n % 3 == 2)
+                return false;
+            n /= 3;
+        }
+        return true;
+    }
+};
+int main(){
+    Solution s;
+    int n;
+    cout<<"Enter the number :-  ";
+    cin>>n;
+    cout<<s.checkPowersOfThree(n);
+    return 0;
+}
