91b730dfd30228c864c2ceb3e1e8f2eb19c0ec2d
Prince242007 <princevaghasiya204@gmail.com>
2026-09-11 22:10:13 +0530

    Daily leetcode  3483 code

---
+++ l_3483.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int totalNumbers(vector<int>& digits)
+    {
+        int count = 0;
+
+        for (int i = 0; i < digits.size(); i++)
+        {
+            // First digit cannot be 0
+            if (digits[i] == 0)
+                continue;
+
+            for (int j = 0; j < digits.size(); j++)
+            {
+                // Cannot use the same index
+                if (j == i)
+                    continue;
+
+                for (int k = 0; k < digits.size(); k++)
+                {
+                    // Cannot use the same index
+                    if (k == i || k == j)
+                        continue;
+
+                    // Last digit must be even
+                    if (digits[k] % 2 != 0)
+                        continue;
+
+                    count++;
+                }
+            }
+        }
+
+        // Remove duplicate numbers
+        sort(digits.begin(), digits.end());
+
+        int ans = 0;
+
+        set<int> numbers;
+
+        for (int i = 0; i < digits.size(); i++)
+        {
+            if (digits[i] == 0)
+                continue;
+
+            for (int j = 0; j < digits.size(); j++)
+            {
+                if (j == i)
+                    continue;
+
+                for (int k = 0; k < digits.size(); k++)
+                {
+                    if (k == i || k == j)
+                        continue;
+
+                    if (digits[k] % 2 != 0)
+                        continue;
+
+                    int num = digits[i] * 100 + digits[j] * 10 + digits[k];
+
+                    numbers.insert(num);
+                }
+            }
+        }
+
+        return numbers.size();
+    }
+};
+int main()
+{
+    Solution s;
+
+    vector<int> digits = {1, 2, 3, 4};
+
+    cout << s.totalNumbers(digits);
+
+    return 0;
+}
