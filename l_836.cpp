db9eb650f01d67571f2ff54b6a4ce4468c88c25d
Prince242007 <princevaghasiya204@gmail.com>
2026-09-14 10:08:00 +0530

    Daily leetcode 836 code

---
+++ l_836.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
+        
+        // Check X-axis overlap
+        if (rec1[2] <= rec2[0] || rec2[2] <= rec1[0])
+            return false;
+        
+        // Check Y-axis overlap
+        if (rec1[3] <= rec2[1] || rec2[3] <= rec1[1])
+            return false;
+        
+        return true;
+    }
+};
+int main()
+{
+    Solution s;
+    vector<int> rec1 = {0, 0, 2, 2}, rec2 = {1, 1, 3, 3};
+    cout << s.isRectangleOverlap(rec1, rec2);
+    return 0;
+}
